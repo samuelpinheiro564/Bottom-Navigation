18c88ee0a5242abcd6477b354b2118a3f68dc557
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-03-06 08:12:51 -0300

    criacao do primeiro compnente

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import dtykr from "./styles";
+
+const Title = () => {
+  return (
+    <View>
+      <Text>Title</Text>
+    </View>
+  );
+};
+
+export default Title;
